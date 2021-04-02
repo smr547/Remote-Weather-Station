8d4c5d3218421e570cb914fdbb2461c07248ec67
Steven Ring <smr@southsky.com.au>
2021-04-03 09:30:29 +1100

    New test program

---
+++ src/arduino/read_voltages/read_voltages.ino
@@ -0,0 +1,43 @@
+
+float batt_V;
+float vin_V;
+float batt_mA;
+float mv_per_count = 0.02063;
+float mA_per_count = 500.0/1024.0;
+
+void setup() {
+  Serial.begin(115200);
+
+}
+
+void loop() {
+  for (;;) {
+    // read the input on analog pin 0:
+    int a0Value = analogRead(A0);
+    batt_V = a0Value * mv_per_count;
+    
+    
+    int a1Value = analogRead(A1);
+    vin_V = a1Value * mv_per_count;
+    int a2Value = analogRead(A2);
+    batt_mA = a2Value * mA_per_count;
+    
+    // print out the value you read:
+    Serial.print("A0 = ");
+    Serial.print(a0Value);
+    Serial.print(", A1 = ");
+    Serial.print(a1Value);
+    Serial.print(", A2 = ");
+    Serial.print(a2Value);
+    Serial.println("");
+    Serial.print("Battery = ");
+    Serial.print(batt_V);
+    Serial.print(" volts, Arduino VIN = ");
+    Serial.print(vin_V);
+    Serial.print(" volts, Battery current = ");
+    Serial.print(batt_mA);
+    Serial.println(" mA");
+    delay(1000);
+  }
+
+}
